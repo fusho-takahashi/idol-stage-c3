8203f9cca8f3ade0ba754e30ea7003d92045ae81
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-15 08:10:57 +0900

    ng g c container/result-container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MyOwnCustomMaterialModule } from './my-own-custom-material/my-own-custom-material.module';
+import { ResultContainerComponent } from './container/result-container/result-container.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ResultContainerComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
