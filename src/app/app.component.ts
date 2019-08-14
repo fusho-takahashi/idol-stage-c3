4da11dccddc646c0a7d2b04d58854ec2faf2228f
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-15 07:54:09 +0900

    remove unused codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'idol-stage-c3';
-}
+export class AppComponent {}
