415b981d8277ad744d397c911c17136ba5dacea6
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-15 19:07:29 +0900

    import MatInputModule

--- src/app/my-own-custom-material/my-own-custom-material.module.ts
+++ src/app/my-own-custom-material/my-own-custom-material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatInputModule],
+  exports: [MatToolbarModule, MatInputModule],
 })
 export class MyOwnCustomMaterialModule {}
