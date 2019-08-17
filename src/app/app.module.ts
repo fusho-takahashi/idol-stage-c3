d3ded4e130ad5465f9994f3ff01d466c9fe37e43
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-17 20:32:50 +0900

    app,moduleにFormsModuleをimport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { MyOwnCustomMaterialModule } from './my-own-custom-material/my-own-custom-material.module';
 import { ResultContainerComponent } from './container/result-container/result-container.component';
@@ -23,6 +24,7 @@ import { TimeFieldComponent } from './components/time-field/time-field.component
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    FormsModule,
     MyOwnCustomMaterialModule,
   ],
   providers: [],
