3c1332075a0ae1b915906f85fa3ae6c3eca872ec
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-17 18:25:53 +0900

    app.moduleにFlexLayoutModuleを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MyOwnCustomMaterialModule } from './my-own-custom-material/my-own-custom-material.module';
 import { ResultContainerComponent } from './container/result-container/result-container.component';
@@ -11,11 +12,17 @@ import { FormContainerComponent } from './container/form-container/form-containe
 import { TimeFieldComponent } from './components/time-field/time-field.component';
 
 @NgModule({
-  declarations: [AppComponent, ResultContainerComponent, FormContainerComponent, TimeFieldComponent],
+  declarations: [
+    AppComponent,
+    ResultContainerComponent,
+    FormContainerComponent,
+    TimeFieldComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MyOwnCustomMaterialModule,
   ],
   providers: [],
