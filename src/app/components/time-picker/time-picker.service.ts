6ed48dd92e9634efdde32c5144921ec8c143b95e
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-18 07:30:46 +0900

    timepicker の open メソットを実装

--- src/app/components/time-picker/time-picker.service.ts
+++ src/app/components/time-picker/time-picker.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector, ComponentRef } from '@angular/core';
+import { Overlay, OverlayConfig } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+
+import { TimePickerComponent } from './time-picker.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimePickerService {
+  constructor(private overlay: Overlay) {}
 
-  constructor() { }
+  open() {
+    const positionStrategy = this.overlay
+      .position()
+      .global()
+      .centerHorizontally()
+      .centerVertically();
+
+    const config = new OverlayConfig({
+      positionStrategy,
+    });
+
+    const overlayRef = this.overlay.create(config);
+
+    const timePickerPortal = new ComponentPortal(TimePickerComponent);
+
+    const containerRef: ComponentRef<TimePickerComponent> = overlayRef.attach(
+      timePickerPortal,
+    );
+  }
 }
