4f78544d133de4578fe803fdbfc8994c222185f7
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-18 08:21:12 +0900

    MatButtonModuleを追加

--- src/app/my-own-custom-material/my-own-custom-material.module.ts
+++ src/app/my-own-custom-material/my-own-custom-material.module.ts
@@ -4,9 +4,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatToolbarModule, MatInputModule, MatIconModule, MatRadioModule],
-  exports: [MatToolbarModule, MatInputModule, MatIconModule, MatRadioModule],
+  imports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonModule,
+  ],
 })
 export class MyOwnCustomMaterialModule {}
