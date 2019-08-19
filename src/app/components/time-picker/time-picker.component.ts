41fc40b36e58421c742b81f7f02506cd63da9f34
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-19 14:14:04 +0900

    既存のnumber-pad-areaを消し、hourとminuteのareaを作成

--- src/app/components/time-picker/time-picker.component.ts
+++ src/app/components/time-picker/time-picker.component.ts
@@ -6,70 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./time-picker.component.scss'],
 })
 export class TimePickerComponent implements OnInit {
-  hour: number;
-  munite: number;
-  inputTarget: 'hour' | 'munite' = 'hour';
+  hour = 0;
+  munite = 0;
+
+  hours = {
+    AM: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    PM: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+  };
+
+  selectedAmPm: 'AM' | 'PM';
+
   constructor() {}
 
-  ngOnInit() {}
-
-  changeTarget(target: 'hour' | 'munite') {
-    this.inputTarget = target;
-  }
-
-  clickNumberPad(num: number) {
-    if (this.inputTarget === 'hour') {
-      if (this.hour === undefined || this.hour > 2) {
-        this.hour = num;
-        if (num > 2) {
-          this.inputTarget = 'munite';
-        }
-      } else {
-        if (this.hour === 0) {
-          if (num === 0) {
-            this.inputTarget = 'munite';
-          } else {
-            this.hour = num;
-          }
-        } else {
-          this.hour = this.hour * 10 + num;
-          this.inputTarget = 'munite';
-        }
-      }
-    } else if (this.inputTarget === 'munite') {
-      if (this.munite === undefined || this.munite > 5) {
-        this.munite = num;
-      } else {
-        this.munite = this.munite * 10 + num;
-      }
-    }
-  }
-
-  increment() {
-    if (this.inputTarget === 'hour') {
-      const inputedHour = this.hour === undefined ? 0 : this.hour;
-      this.hour = (inputedHour + 1) % 24;
-    }
-    if (this.inputTarget === 'munite') {
-      const inputedMunite = this.munite === undefined ? 0 : this.munite;
-      this.munite = (this.munite + 1) % 60;
-    }
-  }
-
-  decrement() {
-    if (this.inputTarget === 'hour') {
-      const inputedHour = this.hour === undefined ? 0 : this.hour;
-      this.hour = (inputedHour + 23) % 24;
-    }
-    if (this.inputTarget === 'munite') {
-      const inputedMunite = this.munite === undefined ? 0 : this.munite;
-      this.munite = (this.munite + 59) % 60;
-    }
+  ngOnInit() {
+    this.selectedAmPm = 'PM';
   }
 
   clearTime() {
-    this.hour = undefined;
-    this.munite = undefined;
-    this.inputTarget = 'hour';
+    this.hour = 0;
+    this.munite = 0;
   }
 }
