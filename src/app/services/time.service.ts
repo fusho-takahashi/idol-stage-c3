0cbe2b2ff00762990bc09214e42a56899f5267fa
fusho-takahashi <zenryaku@fusho-takahashi.me>
2019-08-23 18:06:02 +0900

    query とcommand を追加

--- src/app/services/time.service.ts
+++ src/app/services/time.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { Time, DrinkCourses } from '../domain/models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TimeService {
+  private _entranceTime$ = new BehaviorSubject<Time>(null);
+  private _exitTime$ = new BehaviorSubject<Time>(null);
+  private _drinkCourse$ = new BehaviorSubject<DrinkCourses>('soft');
 
-  constructor() { }
+  get entranceTime$() {
+    return this._entranceTime$.asObservable();
+  }
+
+  get exitTime$() {
+    return this._exitTime$.asObservable();
+  }
+
+  get drinkCourse$() {
+    return this._drinkCourse$.asObservable();
+  }
+
+  constructor() {}
+
+  updateEntranceTime(time: Time) {
+    this._entranceTime$.next({ ...time });
+  }
+
+  updateExitTime(time: Time) {
+    this._exitTime$.next({ ...time });
+  }
+
+  updateDrinkCourse(course: DrinkCourses) {
+    this._drinkCourse$.next(course);
+  }
 }
